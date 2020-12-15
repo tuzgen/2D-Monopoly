3b0cf075f318716814971a57e6b3925beeb52a9d
DESKTOP-P91HJG5\Utku <utkuseyfeli@gmail.com>
2020-12-15 16:39:36 +0300

    Forex manager is broken needs a fix now.

--- 2d-monopoly/src/management/ForexManager.java
+++ 2d-monopoly/src/management/ForexManager.java
@@ -5,9 +5,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ForexManager {
     private Forex forex;
-    private Stack<Integer> dollarTrans = new Stack<Integer>();
-    private Stack<Integer> euroTrans = new Stack<Integer>();
-    private Stack<Integer> frankTrans = new Stack<Integer>();
+    public Stack<Integer> dollarTrans = new Stack<Integer>();
+    public Stack<Integer> euroTrans = new Stack<Integer>();
+    public Stack<Integer> frankTrans = new Stack<Integer>();
 
     ForexManager(){
         forex = new Forex();
@@ -16,7 +16,7 @@ public class ForexManager {
     }
 
     public void resetExRates(){
-        forex.setDollarExRate(1);
+        forex.setDollarExRate(1.5);
         forex.setEuroExRate(1);
         forex.setFrankExRate(1);
     }
@@ -27,29 +27,31 @@ public class ForexManager {
         double randomNumFrank = ThreadLocalRandom.current().nextDouble(-1.0, 1.0 + 1.0);
 
         while (!dollarTrans.empty()) {
-            int dollar = dollarTrans.pop();
-            if (dollar > 3000) {
-                updateExRates("Dollar", forex.getDollarExRate() + (dollar / 25000));
+             int dollar = dollarTrans.pop();
+            if (dollar > 200000) {
+                updateExRates("Dollar", forex.getDollarExRate() / ((forex.getDollarExRate() * dollar) / 228000));
             } else if (dollar > 0) {
-                updateExRates("Dollar", forex.getDollarExRate() + (dollar / 250000));
+                updateExRates("Dollar", forex.getDollarExRate() * 8000 / dollar);
             } else if (dollar > -3000) {
-                updateExRates("Dollar", forex.getDollarExRate() - (dollar / 250000));
+                updateExRates("Dollar", forex.getDollarExRate() + (double)((-1*dollar) / 250000));
             } else {
-                updateExRates("Dollar", forex.getDollarExRate() - (dollar / 25000));
+                updateExRates("Dollar", forex.getDollarExRate() + (double)((-1*dollar) / 25000));
             }
         }
-        updateExRates("Dollar", forex.getDollarExRate() + (randomNumDollar/1000));
+
+
+//        updateExRates("Dollar", forex.getDollarExRate() + (randomNumDollar/1000));
 
         while (!euroTrans.empty()) {
             int euro = euroTrans.pop();
             if (euro > 3000) {
-                updateExRates("Euro", forex.getEuroExRate() + (euro / 25000));
-            } else if (euro > 0) {
-                updateExRates("Euro", forex.getEuroExRate() + (euro / 250000));
-            } else if (euro > -3000) {
-                updateExRates("Euro", forex.getEuroExRate() - (euro / 250000));
-            } else {
                 updateExRates("Euro", forex.getEuroExRate() - (euro / 25000));
+            } else if (euro > 0) {
+                updateExRates("Euro", forex.getEuroExRate() - (euro / 250000));
+            } else if (euro > -3000) {
+                updateExRates("Euro", forex.getEuroExRate() + (euro / 250000));
+            } else {
+                updateExRates("Euro", forex.getEuroExRate() + (euro / 25000));
             }
         }
         updateExRates("Euro", forex.getEuroExRate() + (randomNumEuro/1000));
@@ -57,29 +59,29 @@ public class ForexManager {
         while (!frankTrans.empty()) {
             int frank = frankTrans.pop();
             if (frank > 3000) {
-                updateExRates("Frank", forex.getFrankExRate() + (frank / 25000));
-            } else if (frank > 0) {
-                updateExRates("Frank", forex.getFrankExRate() + (frank / 250000));
-            } else if (frank > -3000) {
-                updateExRates("Frank", forex.getFrankExRate() - (frank / 250000));
-            } else {
                 updateExRates("Frank", forex.getFrankExRate() - (frank / 25000));
+            } else if (frank > 0) {
+                updateExRates("Frank", forex.getFrankExRate() - (frank / 250000));
+            } else if (frank > -3000) {
+                updateExRates("Frank", forex.getFrankExRate() + (frank / 250000));
+            } else {
+                updateExRates("Frank", forex.getFrankExRate() + (frank / 25000));
             }
         }
         updateExRates("Frank", forex.getFrankExRate() + (randomNumFrank/1000));
     }
 
     public void updateExRates(String currency, double exRa){
-        if( currency == "Dollar" )
+        if( currency.equals("Dollar") )
             forex.setDollarExRate(exRa);
 
-        if( currency == "Euro" )
+        if( currency.equals("Euro") )
             forex.setEuroExRate(exRa);
 
-        if(currency == "Try")
+        if(currency.equals("Try"))
             forex.setTryExRate(exRa);
 
-        if( currency == "Frank" )
+        if( currency.equals("Frank"))
             forex.setFrankExRate(exRa);
     }
 
@@ -106,16 +108,15 @@ public class ForexManager {
 }
 
 
-//class Test{
-//    public static void main(String[] args){
-//        ForexManager manager = new ForexManager();
-//        manager.dollarTrans.push(21349);
-//        manager.dollarTrans.push(45529);
-//        manager.dollarTrans.push(30529);
-//
-//        manager.calcSupDemand();
-//
-//        System.out.println(manager.forex.getDollarExRate());
-//
-//    }
-//}
+class Test{
+    public static void main(String[] args){
+        ForexManager manager = new ForexManager();
+        manager.dollarTrans.push(201001);
+
+
+        manager.calcSupDemand();
+
+        System.out.println(manager.getDollarExRate());
+
+    }
+}
