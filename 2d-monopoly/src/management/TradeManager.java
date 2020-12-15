64003ec6026fa520e3db224f7ac87d60cef90e8b
ufukpalpas <ufukpalpas@gmail.com>
2020-12-15 17:17:11 +0300

    Trade and TradeManager classes are created. Trade class is fully implementd. There are todos for TradeManager.

---
+++ 2d-monopoly/src/management/TradeManager.java
@@ -0,0 +1,30 @@
+package management;
+
+import entity.Trade;
+import entity.player.Player;
+
+public class TradeManager {
+
+    public TradeManager getInstance(){
+        //TODO
+        return null;
+    }
+
+    public boolean openTrade(Player owner, Player targetPlayer) {
+        //TODO
+        return false;
+    }
+
+    public boolean checkTrades() {
+        //TODO
+        return false;
+    }
+
+    public void acceptTrade(Trade trade) {
+        //TODO
+    }
+
+    public void denyTrade(Trade trade) {
+        //TODO
+    }
+}
