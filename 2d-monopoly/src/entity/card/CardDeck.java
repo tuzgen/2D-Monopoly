86b3e7c334c61c2587b46c84239ff8fa5d984331
ufukpalpas <ufukpalpas@gmail.com>
2020-12-16 12:57:13 +0300

    CardDeck class is created.

--- 2d-monopoly/src/entity/card/CardDeck.java
+++ 2d-monopoly/src/entity/card/CardDeck.java
@@ -1,4 +1,22 @@
 package entity.card;
 
 public class CardDeck {
+    private Card[] cards;
+
+    public CardDeck() {
+        cards = new Card[10];
+    }
+
+    public void shuffleCard(){
+        //TODO
+    }
+
+    public void drawCard(){
+        //TODO
+    }
+
+    public void createDeck(){
+        //TODO
+    }
+
 }
