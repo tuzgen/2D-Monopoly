a178103a6fa115a15563430e3785e97015355dfc
ufukpalpas <ufukpalpas@gmail.com>
2020-12-17 11:53:32 +0300

    MovementToCardStrategy, MovementByNumCardStrategy are implemented.

--- 2d-monopoly/src/entity/card/MovementByNumCardStrategy.java
+++ 2d-monopoly/src/entity/card/MovementByNumCardStrategy.java
@@ -6,6 +6,7 @@ public class MovementByNumCardStrategy implements CardStrategy{
     @Override
     public void activateCard(Player player, Card card) {
         int amount = card.getAmount();
-        //TODO
+        int currentTileNo = player.getLocation();
+        player.setLocation(currentTileNo + amount);// THIS NEED TO UPDATE THE MAP DO NOT FORGET YOU MAY ADD IT TO THE CHARACTER CLASS
     }
 }
