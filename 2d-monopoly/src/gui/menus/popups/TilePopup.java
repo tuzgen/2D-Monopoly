34adef5d67db5f8822e7e1b9aecf13f97fea5d13
AsyaDoga <asya133@gmail.com>
2020-12-18 15:38:03 +0300

    tile popup

--- 2d-monopoly/src/gui/menus/popups/TilePopup.java
+++ 2d-monopoly/src/gui/menus/popups/TilePopup.java
@@ -5,11 +5,19 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import management.GameManager;
+import entity.player.Player;
 
 public class TilePopup {
 		// Constants
@@ -19,14 +27,20 @@ public class TilePopup {
 		// Methods
 		public static void display(String title, CityTile tile) {
 			// Initiate pop up
-			System.out.println();
 			Stage window = new Stage();
 			window.initStyle(StageStyle.UNDECORATED);
 			window.initModality(Modality.APPLICATION_MODAL);
 			window.setTitle(title);
-			window.setMinWidth(250);
+			window.setMinWidth(300);
+			window.setMinHeight(400);
 
-			Text infoText = new Text(tile.getName() + " Tile no " + tile.getId());
+			Text infoText = new Text("Do you wanna buy " + tile.getName() + "?");
+			infoText.setFill(Color.WHITE);
+			Text mafiaDealText = new Text("Do you wanna deal with mafia to buy "
+					+ tile.getName() + " for " + (int)(tile.getPrice()*(0.8))+"₺ ?");
+			mafiaDealText.setFill(Color.WHITE);
+			Text price = new Text(tile.getPrice() + "₺");
+			price.setFill(Color.WHITE);
 
 			// A button which displays a message and quits
 			Button yesButton = new Button("Return");
@@ -34,11 +48,35 @@ public class TilePopup {
 				System.out.println("Button pressed on pop up");
 				window.close();
 			});
+			Button buyButton = new Button("Buy");
+			buyButton.setOnAction( e -> {
+				System.out.println("Button pressed on pop up");
+				GameManager.getInstance().mapBuyTile(GameManager.getInstance().getTurnOfPlayer(),tile.getId());
+				window.close();
+			});
+			Button mafiaButton = new Button("Deal");
+			mafiaButton.setOnAction( e -> {
+				System.out.println("Button pressed on pop up");
+				window.close();
+			});
+
+			// Image
+			Image house = new Image("file:src/vendor/image/house.png");
+			ImageView houseView = new ImageView(house);
+			//houseView.setX(10);
+			//houseView.setY(10);
+			houseView.setFitHeight(100);
+			houseView.setFitWidth(100);
 
 			// Create layout and add Text and Button
-			VBox layout = new VBox(10);
-			layout.getChildren().addAll(infoText, yesButton);
+			VBox layout = new VBox(20);
+			layout.setBackground(new Background(new BackgroundFill(new Color(0,0,0,1), null, null)));
+			HBox hLayout = new HBox(30);
+			hLayout.setAlignment(Pos.CENTER);
+			layout.getChildren().addAll( houseView, infoText, price, hLayout);
 			layout.setAlignment(Pos.CENTER);
+			hLayout.getChildren().addAll( yesButton, buyButton);
+			layout.getChildren().addAll(mafiaDealText, mafiaButton);
 
 			// Set the scene for the pop up
 			Scene scene = new Scene(layout);
